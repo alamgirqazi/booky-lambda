f1c05a94b8c8369844ba5c6ca4c02de9e3107d02
Alamgir Munir Qazi <alamgir.munir@techavenue.biz>
2020-05-28 17:13:15 +0500

    fixed default module bug

--- functions/books-find.js
+++ functions/books-find.js
@@ -1,4 +1,6 @@
-const goodreads = require("goodreads-api-node");
+// added default because otherwise It wouldn't work
+
+const goodreads = require("goodreads-api-node").default;
 
 const goodreadsKey = process.env.goodreadsKey;
 const goodreadsSecret = process.env.goodreadsSecret;
@@ -7,10 +9,7 @@ const credentials = {
   key: goodreadsKey,
   secret: goodreadsSecret,
 };
-// modern JS style - encouraged
 exports.handler = async (event, context) => {
-  console.log("credentials", credentials);
-  //   const gr = goodreads(myCredentials);
   const query = event.queryStringParameters.query || "world";
   if (event.httpMethod !== "GET") {
     return { statusCode: 405, body: "Method Not Allowed" };
@@ -18,14 +17,11 @@ exports.handler = async (event, context) => {
 
   try {
     const gr = goodreads(credentials);
-    console.log("gr", gr);
-    console.log("credentials", credentials);
     const response = await gr.searchBooks({
       q: query,
       page: 1,
       // field: "title",
     });
-    console.log("resp");
     return {
       statusCode: 200,
       body: JSON.stringify(response.search),
--- webpack.config.js
+++ webpack.config.js
@@ -1,6 +1,5 @@
 const Dotenv = require("dotenv-webpack");
 
-// @see https://github.com/netlify/netlify-lambda#webpack-configuration
 module.exports = {
   plugins: [new Dotenv()],
 };
