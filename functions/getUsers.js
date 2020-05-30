e87fb1c2eae6205303c208f445e7450bc955c28c
Alamgir Munir Qazi <alamgir.munir@techavenue.biz>
2020-05-30 11:23:52 +0500

    fix cors

--- functions/getUsers.js
+++ functions/getUsers.js
@@ -3,7 +3,6 @@ export async function handler(event, context) {
     statusCode: 200,
     headers: {
       "Access-Control-Allow-Origin": "*", // Required for CORS support to work
-      "Access-Control-Allow-Credentials": true, // Required for cookies, authorization headers with HTTPS
     },
     body: JSON.stringify({
       message: `Hello world ${Math.floor(Math.random() * 10)}`,
